c71e0e6b6f68eef9d83d74ee150d6bfaaaaf2bf3
OhulchanskiyKHAI <y.a.ohulchanskyi@student.khai.edu>
2023-01-04 11:14:50 +0200

    chatbot.py 2 stage

--- chatbot.py
+++ chatbot.py
@@ -1,2 +1,4 @@
-print("Hello! My name is Chat_Bot."
-      "I was created in 2022")
+print("Hello! My name is Chat_Bot.")
+print("I was created in 2022")
+print("Please,remind your name.")
+print("What a great name you have, Evgeniy")
